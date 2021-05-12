4f929b77fa7589ebf03ea0840ab08d62ea52ccf4
Siddharth Bhat <siddu.druid@gmail.com>
2021-05-12 10:32:15 +0530

    get peano nats

--- tiark.py
+++ tiark.py
@@ -26,7 +26,7 @@ def struct(e):
 # printing
 def stringify(e):
     global nVars
-    save = nVars
+    save = int(nVars)
     return json.dumps(struct(e))
     nVars = save
 
@@ -100,6 +100,7 @@ test = fun(lambda x: app(x, lambda y: y))
 print(stringify(test))
 
 # ===STLC===
+print("===STLC===")
 
 def isfun0(f): return callable(f)
 def fun0(f): assert(isfun0(f)); return f
@@ -133,8 +134,8 @@ def fun(atp, f):
 
 
 def app(f,a):
-  let [fv, ftp] = untyped(f)
-  let [av, atp] = untyped(a)
+  [fv, ftp] = untyped(f)
+  [av, atp] = untyped(a)
   assert isfuntype(ftp), f"not a function: {f}"
 
   # paramty, argty
@@ -153,6 +154,97 @@ nVars = 0
 intId = fun("Int", lambda x: x)
 printt(intId)
 
+nVars = 0
+eta = fun(funtype("Int","Int"), lambda f: fun("Int", lambda x: app(f,x)))
+printt(eta)
+etaIntId = app(eta, fun("Int", lambda x: x))
+printt(etaIntId)
 
+# ===Dependent types (COC)===
+print("===COC===")
 
-# ===Dependent type===
+def isfun0(f): return callable(f)
+def fun0(f): assert(isfun0(f)); return f
+def app0(f,a):
+    if (isfun0(f)): return f(a)
+    else: return [f, a]
+
+def forall0(atp,f): return ["forall", atp, f] 
+def isforall0(f): return isinstance(f, list) and f[0] == "forall"
+
+def typed(e,t):
+    # create a real function.
+    o = lambda a: app(o,a)
+    o.untyped = e; o.type = t
+    return o
+
+def istyped(e): return hasattr(e, "untyped") and hasattr(e, "type")
+def untyped(e): assert istyped(e), f"no type: {e}"; return [e.untyped, e.type]
+
+def forall(atp,f):
+    [atpt, atpk] = untyped(atp)
+    assert atpk == "Type" or atpk == "Kind", f"illegal arg type/kind: {atpk}"
+    [res, tpe] = untyped(f(typed("x?",atpt)))
+    return typed(forall0(atpt, lambda x: untyped(f(typed(x,atpt)))[0]), tpe)
+
+def fun(atp,f):
+    [atpt,atpk] = untyped(atp)
+    assert atpk == "Type" or atpk == "Kind", f"illegal arg type/kind: {atpk}"
+    return typed(
+          fun0(lambda x: untyped(f(typed(x,atpt)))[0]),
+          forall0(atpt, lambda x: untyped(f(typed(x,atpt)))[1]))
+
+def app(f,a):
+    [f1,ftp] = untyped(f)
+    [a1,atp] = untyped(a)
+    assert(isforall0(ftp), f"not a function: {f}")
+    [key,ptp,frtp] = ftp
+
+    # TODO: why do I need this hack..?
+    global nVars
+    nVars = 0; str_atp = stringify(atp)
+    nVars = 0; str_ptp = stringify(ptp)
+    assert  str_atp == str_ptp, f"type mismatch: {str_atp} != {str_ptp}"
+    res = app0(f1, a1)
+    rtp = frtp(a1)
+    return typed(res,rtp)
+
+def constant(tm,ty):
+    [tyt,tyk] = untyped(ty)
+    assert tyk == "Type" or tyk == "Kind", f"illegal arg type/kind: {tyk}"
+    return typed(tm,tyt)
+Type = typed("Type","Kind")
+
+print("typeId:")
+nVars = 0
+typeId = fun(Type, lambda t: t)
+printt(typeId)
+
+print("polyId:")
+nVars = 0
+polyId = fun(Type, lambda t: fun(t, lambda x: x))
+printt(polyId)
+
+N = constant("N",Type)
+z = constant("z",N)
+s = constant("s",forall(N, lambda x: N))
+
+three = s(s(s(z)))
+print("three:")
+printt(three)
+
+ff = lambda T: forall(T, lambda x: T) # fun type T->T
+ChurchT = forall(Type, lambda N: forall(N, lambda z: forall(ff(N), lambda s: N)))
+ChurchN = lambda f: fun(Type, lambda N: fun(N, lambda z: fun(ff(N), lambda s: f(N)(z)(s))))
+peano = fun(ChurchT, lambda n: n(N)(z)(s))
+
+print("church three:")
+nVars = 0
+threeChurch = ChurchN(lambda N:  lambda z: lambda s: s(s(s(z))))
+printt(threeChurch)
+
+print("peano three:")
+printt(peano(threeChurch))
+
+# this chokes
+# plus = fun(ChurchT, lambda a: fun(ChurchT, lambda b: ChurchN(lambda N: lambda z: lambda s: return a(N)(b(N)(z)(s))(s))))
