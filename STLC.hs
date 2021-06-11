801823308ef211df4fbcc1676d6ac37630eab65d
Siddharth Bhat <siddu.druid@gmail.com>
2021-06-11 09:38:34 +0530

    get LSP working

--- STLC.hs
+++ STLC.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Language.LSP.Server
+import Language.LSP.Types
+import Control.Monad.IO.Class
+import qualified Data.Text as T
+
+
 -- Simply typed lambda calculus with
 -- normalization by evaluation.
 -- https://en.wikipedia.org/wiki/Normalisation_by_evaluation
@@ -99,12 +107,8 @@ toDecl tuple = do
 toToplevel :: AST -> Either Error [Decl]
 toToplevel ast = tuplefor toDecl ast
 
-main :: IO ()
-main = do
-  args <- getArgs
-  path <- case args of
-           [path] -> pure path
-           _ -> (print "expected single file path to parse") >> exitFailure
+mainFile :: String -> IO ()
+mainFile path = do
   file <- readFile path
   putStrLn $"file contents:"
   putStrLn $ file
@@ -130,6 +134,57 @@ main = do
             putStr $ "  output: "
             putStrLn $ show outstx
   return ()
+
+lspHandlers :: Handlers (LspM ())
+lspHandlers = mconcat
+  [ notificationHandler SInitialized $ \_not -> do
+      let params = ShowMessageRequestParams MtInfo "Turn on code lenses?"
+            (Just [MessageActionItem "Turn on", MessageActionItem "Don't"])
+      _ <- sendRequest SWindowShowMessageRequest params $ \res ->
+        case res of
+          Right (Just (MessageActionItem "Turn on")) -> do
+            let regOpts = CodeLensRegistrationOptions Nothing Nothing (Just False)
+              
+            _ <- registerCapability STextDocumentCodeLens regOpts $ \_req responder -> do
+              let cmd = Command "Say hello" "lsp-hello-command" Nothing
+                  rsp = List [CodeLens (mkRange 0 0 0 100) (Just cmd) Nothing]
+              responder (Right rsp)
+            pure ()
+          Right _ ->
+            sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Not turning on code lenses")
+          Left err ->
+            sendNotification SWindowShowMessage (ShowMessageParams MtError $ "Something went wrong!\n" <> T.pack (show err))
+      pure ()
+  , requestHandler STextDocumentHover $ \req responder -> do
+      let RequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
+          Position _l _c' = pos
+          rsp = Hover ms (Just range)
+          ms = HoverContents $ markedUpContent "lsp-demo-simple-server" "Hello world"
+          range = Range pos pos
+      responder (Right $ Just rsp)
+  ]
+
+
+mainLSP :: IO ()
+mainLSP = do
+  putStrLn $ "launching LSP server..."
+  runServer $ ServerDefinition
+    { onConfigurationChange = const $ pure $ Right ()
+    , doInitialize = \env _req -> pure $ Right env
+    , staticHandlers = lspHandlers
+    , interpretHandler = \env -> Iso (runLspT env) liftIO
+    , options = defaultOptions
+    }
+  return ()
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+   [] -> mainLSP
+   ["lsp"] -> mainLSP
+   [path] -> mainFile path
+   _ -> (putStrLn "ERROR: expected single file path to parse, or 'lsp' to launch server") >> exitFailure
 
 
 -- | type directed semantic lifting
